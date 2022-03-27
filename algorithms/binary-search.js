9be100f45e216ec6db8b313ef567c2ebd96b4525
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-03-27 14:41:13 +0300

    add new implementation of binary search

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -1,6 +1,6 @@
 // binary search
 
-const array = [1, 2 , 3, 4, 5, 6, 7, 8, 9];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //O(n);
 function searchElement(arr, elem) {
@@ -14,28 +14,52 @@ function searchElement(arr, elem) {
 }
 
 // O(log n)
-function binarySearchElement(arr, elem) {
-  let start = 0;
-  let end = arr.length;
+// function binarySearchElement(arr, elem) {
+//   let start = 0;
+//   let end = arr.length - 1;
+//
+//   while (end - start > 1) {
+//     const mid = Math.floor((start + end) / 2);
+//
+//     if (arr[mid] === elem) {
+//       return mid;
+//     }
+//
+//     if (arr[mid] > elem) {
+//       end = mid;
+//     } else {
+//       start = mid;
+//     }
+//   }
+//   return -1;
+// }
 
-  while (end - start > 1) {
-    const mid = Math.floor((start + end) / 2);
+function binarySearch(arr, elem) {
+  let low = 0;
+  let high = arr.length - 1;
 
-    if (arr[mid] === elem) {
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = arr[mid];
+
+    if (guess === elem) {
       return mid;
     }
-
-    if (arr[mid] > elem) {
-      end = mid;
+    if (guess < elem) {
+      low = mid + 1;
     } else {
-      start = mid;
+      high = mid - 1;
     }
   }
+
   return -1;
 }
 
+console.log(binarySearch(array, 6));
+console.log(binarySearch(array, 11));
 
-console.log(searchElement(array, 2));
-console.log(binarySearchElement(array, 2));
-console.log(searchElement(array, 11));
-console.log(binarySearchElement(array, 11));
+
+// console.log(searchElement(array, 2));
+// console.log(binarySearchElement(array, 2));
+// console.log(searchElement(array, 11));
+// console.log(binarySearchElement(array, 11));
