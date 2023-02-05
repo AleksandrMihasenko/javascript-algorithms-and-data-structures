fe8743bf4e2c143b82b8b744b189ecfaa897d1f6
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2023-02-05 17:40:13 +0400

    add task solution "contains duplicates"

---
+++ leetcode/easy/contains_duplicate/contains_duplicate.js
@@ -0,0 +1,33 @@
+
+// Solution 1
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+function containsDuplicateVersionOne(nums) {
+  const setNums = new Set(nums);
+
+  return setNums.size !== nums.length;
+}
+
+// Solution 2 with hash map
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+function containsDuplicateVersionTwo(nums) {
+  const hashMap = {};
+  let result = false
+
+  for (const [_, value] of nums.entries()) {
+    if (hashMap[value]) return true;
+
+    hashMap[value] = hashMap[value] + 1 || 1;
+  }
+
+  return result;
+}
+
+console.log(containsDuplicateVersionOne([1, 2, 3, 4, 5]));
+console.log(containsDuplicateVersionOne([1, 2, 3, 4, 5, 1]));
+
+console.log(containsDuplicateVersionTwo([1, 2, 3, 4, 5]));
+console.log(containsDuplicateVersionTwo([1, 2, 3, 4, 5, 1]));
